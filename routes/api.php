6c8d8dd40e9c30524ee8d0deb6810a7e9de0dfd3
ankitjain28may <ankitjain28may77@gmail.com>
2017-02-06 01:52:24 +0530

    Added CORS

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/leaderboard', 'Github\LeaderboardController@index');
+Route::middleware('cors')->get('/leaderboard', 'Github\LeaderboardController@index');
