bbdddd83857af28d79b407fd6be0b0283130df8f
ankitjain28may <ankitjain28may77@gmail.com>
2017-02-07 19:52:50 +0530

    Testing env

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('/api/github/user/callback', 'Github\GitAuthController@callback');
 
 Route::resource('/api/guzz', 'Github\ApiController');
 
-// Route::get('/{any}', function() {
-//     return View('error');
-// });
+Route::get('/{any}', function() {
+    return env('APP_ENV');
+});
 
